a7c819e2eafb16bc7fb54985ad7167616d0bec26
skyhilam <skyhilam@gmail.com>
2026-02-23 18:13:35 +0800

    fix: replace deprecated UNotifications with UApp root wrapper

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <div class="min-h-screen bg-gray-50 dark:bg-gray-900">
+      <NuxtPage />
+    </div>
+  </UApp>
 </template>
